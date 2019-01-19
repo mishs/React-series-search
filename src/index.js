aa05fcb30ad6b841fa827310c8cb98393391c90e
Mish Siwela <mishuct26@gmail.com>
2019-01-19 16:38:30 +0200

    Sync'd

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const greetings = <h1>Hey eer ffy te</h1>;
+const greetings = <h1>Hey</h1>;
 ReactDOM.render(greetings, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
