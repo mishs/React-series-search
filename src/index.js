b3cb6c029f6e7a50e6c9792c2e95f5ffa154934d
Mish Siwela <mishuct26@gmail.com>
2019-01-19 17:19:54 +0200

    JSX intro

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const greetings = <h1>Hey</h1>;
+// const greetings = <h1>Hey</h1>;
+// JSX
+
+const getCurrentDate = () => {
+  const date = new Date();
+  return date.toDateString();
+};
+// JSX- uses curly brackets (inside HTML too)
+const greetings = <h1>Hi, today is: {getCurrentDate()}</h1>;
 ReactDOM.render(greetings, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
